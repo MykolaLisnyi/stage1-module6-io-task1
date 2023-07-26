13f537e89de4c34d87acf3612163532c573c2e33
Mykola Lisnyi <lesnoy.nikolay@gmail.com>
2023-07-26 19:12:20 +0300

    1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,33 @@
 package com.epam.mjc.io;
 
 import java.io.File;
-
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+
+        try (java.io.FileReader fileReader = new java.io.FileReader (file)) {
+            int ch;
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((ch = fileReader.read()) != -1) {
+                stringBuilder.append((char) ch);
+            }
+            StringTokenizer data = new StringTokenizer(stringBuilder.toString()
+                    .replaceAll("Name:", " ")
+                    .replaceAll("Age:", " ")
+                    .replaceAll("Email:", " ")
+                    .replaceAll("Phone:", " "));
+            profile.setName(data.nextToken());
+            profile.setAge(Integer.parseInt(data.nextToken()));
+            profile.setEmail(data.nextToken());
+            profile.setPhone(Long.parseLong(data.nextToken()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return profile;
     }
 }
